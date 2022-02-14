fe2fce6aaad95bd435c57b4d42902826ed13a60d
mcsrk <49255542+mcsrk@users.noreply.github.com>
2022-02-13 21:14:05 -0500

    axios and 1st get req

--- react-test/pages/MainPage.js
+++ react-test/pages/MainPage.js
@@ -1,32 +1,93 @@
-import React from "react";
-import { Breadcrumb } from "antd";
+import React, { useEffect, useState } from "react";
+import { Breadcrumb, Col, notification, Radio, Row, Typography } from "antd";
 import Layout, { Content, Footer } from "antd/lib/layout/layout";
 import SearchBar from "./SearchBar";
-import ComicCard from "./ComicCard";
-// import Background from "./images/bg.jpg";
-
+import ComicList from "./ComicList";
+import axios from "axios";
+const { Title } = Typography;
 const MainPage = () => {
+  const [input, setInput] = useState("");
+  const [comicsData, setComicsData] = useState([]);
+
+  const useDebouncedEffect = (effect, deps, delay) => {
+    useEffect(() => {
+      const handler = setTimeout(() => effect(), delay);
+      return () => clearTimeout(handler);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [...(deps || []), delay]);
+  };
+  const openNotification = () => {
+    notification.error({
+      message: `Error en la búsqueda`,
+      description: "Recargue la página si el error pesrsiste",
+    });
+  };
+
+  const getComicsByInput = async () => {
+    const TS = "1";
+    const APIKEY = "22d97eb6512779e32a83d97ad7e7ff41";
+    const HASH = "fe011abff2ca533eb29fff528cc613d9";
+    try {
+      let res = await axios.get(`http://gateway.marvel.com/v1/public/comics`, {
+        params: { ts: TS, apikey: APIKEY, hash: HASH },
+      });
+      // setComicsData(res);
+      console.warn(res.data.data);
+    } catch (err) {
+      console.error(err);
+      openNotification();
+    }
+  };
+
+  useDebouncedEffect(getComicsByInput, [input], 250);
+
   return (
     <>
       <Layout>
         <Content>
-          <SearchBar />
+          <SearchBar setInput={setInput} input={input} />
 
           <div style={{ padding: "0 50px" }}>
-            <Breadcrumb style={{ margin: "16px 0" }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
-            </Breadcrumb>
-            <div
-              style={{
-                minHeight: "280px",
-                padding: "24px",
-                background: "#fff",
-              }}
-            >
-              <ComicCard />
-            </div>
+            <Col>
+              <Row
+                style={{
+                  marginTop: "16px",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography>
+                  <Title
+                    level={5}
+                    style={{
+                      textAlign: "center",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Filtra por personajes
+                  </Title>
+                </Typography>
+              </Row>
+              <Row>
+                <Radio.Group
+                  defaultValue="spiderman"
+                  style={{ margin: "12px auto 0px auto" }}
+                >
+                  <Radio.Button value="spiderman">SpiderMan</Radio.Button>
+                  <Radio.Button value="Wolverine">Wolverine</Radio.Button>
+                  <Radio.Button value="Hulk">Hulk</Radio.Button>
+                  <Radio.Button value="Thor">Thor</Radio.Button>
+                </Radio.Group>
+              </Row>
+              <Row>
+                <Breadcrumb style={{ margin: "16px 0" }}>
+                  <Breadcrumb.Item>Home</Breadcrumb.Item>
+                  <Breadcrumb.Item>List</Breadcrumb.Item>
+                  <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+              </Row>
+            </Col>
+
+            <ComicList />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
