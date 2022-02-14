095480876a108ffc2149e74a091019c8285c5a13
mcsrk <49255542+mcsrk@users.noreply.github.com>
2022-02-13 21:19:42 -0500

    actually a real list

--- react-test/pages/ComicList.js
+++ react-test/pages/ComicList.js
@@ -1,43 +1,55 @@
-import { Col, Row } from "antd";
+import { Col, List, Row } from "antd";
 import React from "react";
 import ComicCard from "./ComicCard";
+const data = [
+  {
+    title: "Title 1",
+    author: "Jhon",
+    price: "299",
+  },
+  {
+    title: "Title 2",
+    author: "Jhon",
+    price: "299",
+  },
+  {
+    title: "Title 3",
+    author: "Jhon",
+    price: "299",
+  },
+  {
+    title: "Title 4",
+    author: "Jhon",
+    price: "299",
+  },
+  {
+    title: "Title 5",
+    author: "Jhon",
+    price: "299",
+  },
+  {
+    title: "Title 6",
+    author: "Jhon",
+    price: "299",
+  },
+];
 
 const ComicList = ({ comicData }) => {
   return (
-    <Row
+    <List
       style={{
         minHeight: "280px",
         padding: "24px",
         background: "#fff",
       }}
-      gutter={[24, 24]}
-    >
-      <Col span={6}>
-        <ComicCard />
-      </Col>
-      <Col span={6}>
-        <ComicCard />
-      </Col>
-      <Col span={6}>
-        <ComicCard />
-      </Col>
-      <Col span={6}>
-        <ComicCard />
-      </Col>
-
-      <Col span={6}>
-        <ComicCard />
-      </Col>
-      <Col span={6}>
-        <ComicCard />
-      </Col>
-      <Col span={6}>
-        <ComicCard />
-      </Col>
-      <Col span={6}>
-        <ComicCard />
-      </Col>
-    </Row>
+      grid={{ gutter: 16, column: 4 }}
+      dataSource={data}
+      renderItem={(ele) => (
+        <List.Item>
+          <ComicCard title={ele.title} author={ele.author} price={ele.price} />
+        </List.Item>
+      )}
+    />
   );
 };
 
